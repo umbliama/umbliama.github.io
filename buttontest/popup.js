dc83ca945d1a4a2f7c790dd4825144ce2bf8f804
Umblia Maxim <umblia34@gmail.com>
2019-01-28 15:25:33 +0300

    Test btn1

--- buttontest/popup.js
+++ buttontest/popup.js
@@ -1,10 +1,10 @@
-function PlaySound(soundobj) {
-  var thissound = document.getElementById(soundobj);
+function PlaySound(soundobj1) {
+  var thissound = document.getElementById(soundobj1);
   thissound.play();
 }
 
-function StopSound(soundobj) {
-  var thissound = document.getElementById(soundobj);
+function StopSound(soundobj1) {
+  var thissound = document.getElementById(soundobj1);
   thissound.pause();
   thissound.currentTime = 0;
 }
