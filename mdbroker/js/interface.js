03411d534cbf42aafbc93cf3727fd17f94b9f1b1
Maxim Umblia <umblia34@gmail.com>
2020-05-02 21:18:18 +0700

    fixes

--- mdbroker/js/interface.js
+++ mdbroker/js/interface.js
@@ -43,32 +43,3 @@ $(".js-close-filtres").on("click", function () {
   $(".js-filtres").removeClass("hidden");
   $(".section-filters-content-show__btn--close-filtres").removeClass("hidden");
 });
-if ($("#metroModal").length > 0) {
-  var modal = document.getElementById("metroModal");
-
-  // Get the button that opens the modal
-  var btn = document.querySelector(".js-metro");
-
-  // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
-
-  // When the user clicks on the button, open the modal
-  btn.onclick = function () {
-    modal.style.display = "block";
-  };
-
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function () {
-    modal.style.display = "none";
-  };
-
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function (event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  };
-}
-$(".js-choose-type").on("click", function () {
-  $(this).toggleClass("active");
-});
--- mdbroker/js/modal.js
+++ mdbroker/js/modal.js
@@ -26,3 +26,32 @@ document.addEventListener("DOMContentLoaded", function (event) {
     }
   };
 });
+if ($("#metroModal").length > 0) {
+  var modal = document.getElementById("metroModal");
+
+  // Get the button that opens the modal
+  var btn = document.querySelector(".js-metro");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on the button, open the modal
+  btn.onclick = function () {
+    modal.style.display = "block";
+  };
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+}
+$(".js-choose-type").on("click", function () {
+  $(this).toggleClass("active");
+});
