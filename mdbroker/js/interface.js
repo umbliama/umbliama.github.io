802c0475c53946ba08370270ca91885b10ba8f42
Maxim Umblia <umblia34@gmail.com>
2020-05-02 18:35:33 +0700

    fix header

--- mdbroker/js/interface.js
+++ mdbroker/js/interface.js
@@ -6,17 +6,18 @@ $(".js-menu-close").on("click", function () {
   $(".section-header-content-mobile-menu").removeClass("active");
   $(".section-header-content").css("height", "100%");
 });
-$("#slider-range").slider({
-  range: true,
-  min: 0,
-  max: 10000,
-  values: [0, 10000],
-  slide: function (event, ui) {
-    $("#amount").val(ui.values[0]);
-    $("#amount2").val(ui.values[1]);
-  },
-});
-
+if ($("#slider-range").length > 0) {
+  $("#slider-range").slider({
+    range: true,
+    min: 0,
+    max: 10000,
+    values: [0, 10000],
+    slide: function (event, ui) {
+      $("#amount").val(ui.values[0]);
+      $("#amount2").val(ui.values[1]);
+    },
+  });
+}
 $("#amount").change(function () {
   $("#slider-range").slider("values", 0, $(this).val());
 });
@@ -37,30 +38,32 @@ $(".js-close-filtres").on("click", function () {
   $(".js-filtres").removeClass("hidden");
   $(".section-filters-content-show__btn--close-filtres").removeClass("hidden");
 });
-var modal = document.getElementById("metroModal");
+if ($("#metroModal").length > 0) {
+  var modal = document.getElementById("metroModal");
 
-// Get the button that opens the modal
-var btn = document.querySelector(".js-metro");
+  // Get the button that opens the modal
+  var btn = document.querySelector(".js-metro");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
-btn.onclick = function () {
-  modal.style.display = "block";
-};
+  // When the user clicks on the button, open the modal
+  btn.onclick = function () {
+    modal.style.display = "block";
+  };
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
     modal.style.display = "none";
-  }
-};
+  };
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+}
 $(".js-choose-type").on("click", function () {
   $(this).toggleClass("active");
 });
